ab3ee26b5d07b1f620f62d501738da147eeccfe3
aster <more.alexandre11@gmail.com>
2023-02-25 00:47:36 -0300

    novo projeto

---
+++ dio_java_boot/src/dio_loops/Ex2_Nota.java
@@ -0,0 +1,21 @@
+package dio_loops;
+
+import java.util.Scanner;
+
+public class Ex2_Nota {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int nota;
+		
+		System.out.println("Nota: ");
+		nota = sc.nextInt();
+		
+		while(nota < 0 | nota > 10) {
+			System.out.println("Nota invalida, digite novamente: ");
+			nota = sc.nextInt();
+		}
+
+	}
+
+}
